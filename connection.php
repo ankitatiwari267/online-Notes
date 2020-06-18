da98b086a6c6330591ac67a2ebfbd92a1eb60667
ankitatiwari267 <tiwariankita142@gmail.com>
2020-06-18 18:56:52 +0530

    initial commit

---
+++ connection.php
@@ -0,0 +1,7 @@
+<?php
+$link=mysqli_connect("sql109.epizy.com","epiz_26034353","j5LUeSizCOh3","epiz_26034353_ankitadatabase");
+if(mysqli_connect_error()){
+  die("ERROR: Unable to connect:".mysqli_connect_error());
+  echo"<script>window.alert('Hi!')</script>";
+}
+  ?>
